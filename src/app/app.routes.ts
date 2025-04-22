62895ddbb2708426550333b25e26104fe736e4ad
Mateus Jesus <pcp.ios@daten.com.br>
2025-04-22 14:36:22 -0300

    alteraçoes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { TelaAdminComponent } from './admin/tela-admin/tela-admin.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
