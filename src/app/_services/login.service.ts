01847582836c4c432d7f61bad35d34439f4def7e
Mateus Jesus <pcp.ios@daten.com.br>
2025-04-22 13:15:50 -0300

    Toda tela e serviço de login

---
+++ src/app/_services/login.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../_models/User';
+import { Login } from '../_models/Login';
+import { map, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService extends BaseService {
+  private api = environment.apiPrincipal;
+  private httpClient = inject(HttpClient);
+
+  login(login: Login): Observable<User | null> {
+    return this.httpClient
+      .get<User[]>(`${this.api}users?usuario=${login.usuario}&senha=${login.senha}&tipoUsuario=${login.tipoUsuario}`)
+      .pipe(
+        map((users: User[]) => users.length > 0 ? users[0] : null),
+        tap((user) => {
+          if (user) {
+            sessionStorage.setItem("usuario", user.usuario);
+            sessionStorage.setItem("nome", user.nome);
+            sessionStorage.setItem("tipoUsuario", user.tipoUsuario);
+          }
+        })
+      );
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,45 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/User';
-import { UserServiceService } from '../_services/user-service.service';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { LoginService } from '../_services/login.service';
 import { Login } from '../_models/Login';
-import { RouterModule } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [  CommonModule,
+  imports: [CommonModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule],
+    ReactiveFormsModule
+  ],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit {
-  public user! :User;
-  form: FormGroup = new FormGroup({});
-
+export class LoginComponent {
   labelUser: string = "Email";
   placeHolderUser: string = "Email";
+  user!: User;
+  // form: FormGroup = new FormGroup({});
+  loginForm!: FormGroup;
 
 
-constructor(private fb: FormBuilder, private userService: UserServiceService){}
-
-  ngOnInit(): void {
-    this.user = new User();
-    this.inicializarFormulario();
-  }
-
-
-  inicializarFormulario(): void {
-    this.form = this.fb.group({
-      tipoUsuario : ['', [Validators.required]],
-      usuario : ['', [Validators.required]],
-      senha : ['', [Validators.required]]
-    });
+  constructor(private loginService: LoginService, private router: Router, private toastService: ToastrService) {
+    this.loginForm = new FormGroup({
+      tipoUsuario: new FormControl('', [Validators.required]),
+      usuario: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
   }
 
   tipoUsuarioLabel(event: Event): void {
@@ -55,20 +48,47 @@ constructor(private fb: FormBuilder, private userService: UserServiceService){}
     }
   }
 
-  logar(): void{
-    if(this.form.invalid){
-      alert("usuario ou senha incorretos");
-      return;
-    }
-
+  submit() {
     const login: Login = {
-      usuario: this.form.value.usuario,
-      senha: this.form.value.senha,
-      tipoUsuario: this.form.value.tipoUsuario
+      usuario: this.loginForm.value.usuario,
+      senha: this.loginForm.value.senha,
+      tipoUsuario: this.loginForm.value.tipoUsuario
     };
 
-    var user = this.userService.buscaLogin(login);
+    this.loginService.login(login).subscribe({
+      next: (user: User | null) => {
+        if (!user) {
+          this.toastService.error("Usuário, senha ou tipo de usuário incorreto");
+        } else {
+          console.log("Tipo de usuário: ", user.tipoUsuario);  // Verifique o valor aqui
 
-    alert(user);
+          switch (user.tipoUsuario.trim().toLowerCase()) {
+            case 'ps':
+              this.toastService.success("Login feito com sucesso!");
+              console.log("Redirecionando para a página inicial...");
+              setTimeout(() => this.navigate(''), 0);  // Atrasar a navegação por um ciclo de evento
+              break;
+            case 'pc':
+              console.log("Redirecionando para admin/inicio...");
+              this.navigate('admin/inicio');
+              break;
+            case 'pa':
+              console.log("Redirecionando para admin/inicio...");
+              this.navigate('admin/inicio');
+              break;
+            default:
+              this.toastService.error("Tipo de usuário desconhecido, tente novamente mais tarde!");
+              break;
+          }
+        }
+      },
+      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
+    });
+  }
+
+
+
+  navigate(rota: string) {
+    this.router.navigateByUrl(`/${rota}`);
   }
 }
