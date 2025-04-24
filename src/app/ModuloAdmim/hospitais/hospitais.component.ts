69432f92deb837894a6c5080dcb8d2c0a8cd0c29
Mateus-Jesus-MJ <157847342+Mateus-Jesus-MJ@users.noreply.github.com>
2025-04-24 20:39:32 -0300

    Ajustes na aba de hospitais

--- src/app/ModuloAdmim/hospitais/hospitais.component.ts
+++ src/app/ModuloAdmim/hospitais/hospitais.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core'; // Importe OnInit
 import { FormGroup } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-hospitais',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './hospitais.component.html',
   styleUrl: './hospitais.component.scss'
 })
-export class HospitaisComponent {
+export class HospitaisComponent implements OnInit { // Implemente OnInit
+  rotaFilhaAtiva = false;
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void { // Use o ngOnInit para a lógica inicial
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.verificarRotaFilhaAtiva(); // Chame a função de verificação
+      });
+
+    this.verificarRotaFilhaAtiva(); // Verifique na inicialização também
+  }
+
+  private verificarRotaFilhaAtiva(): void {
+    this.rotaFilhaAtiva = this.route.children.length > 0;
+  }
 }
