a4d1b2a223e716046b904706e3b012898efdd13f
Mateus-Jesus-MJ <157847342+Mateus-Jesus-MJ@users.noreply.github.com>
2025-07-06 16:09:39 -0300

    Filtro de intenamentos

--- src/app/ModuloAdmim/internamentos/internamentos.component.ts
+++ src/app/ModuloAdmim/internamentos/internamentos.component.ts
@@ -7,10 +7,12 @@ import { AuthService } from '../../_services/auth.service';
 import { filter } from 'rxjs';
 import { IntenamentoService } from '../../_services/intenamento.service';
 import { Internamento } from '../../_models/internamento';
+import { PaginacaoComponent } from "../_components/paginacao/paginacao.component";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-internamentos',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, PaginacaoComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './internamentos.component.html',
   styleUrl: './internamentos.component.scss'
 })
@@ -19,7 +21,12 @@ export class InternamentosComponent implements OnInit {
   userPodeIncluir = false;
   userPodeEditar = false;
   userPodeExcluir = false;
-  internamentos: Internamento[] = [];
+  dados: Internamento[] = [];
+  dadosFiltrados: any[] = [];
+  dadosPaginados: any[] = [];
+  textoFiltro: string = '';
+  paginaAtual = 1;
+  itensPorPagina = 10;
 
   constructor(private router: Router,
     private route: ActivatedRoute,
@@ -52,7 +59,7 @@ export class InternamentosComponent implements OnInit {
   private verificarRotaFilhaAtiva(): void {
     this.rotaFilhaAtiva = this.route.children.length > 0;
   }
-  
+
   userPermissoes() {
     this.userPodeIncluir = this.temPermissao('hospitais', 'incluir');
     this.userPodeEditar = this.temPermissao('hospitais', 'editar');
@@ -74,18 +81,74 @@ export class InternamentosComponent implements OnInit {
     );
   }
 
-  buscarInternamentos(){
+  buscarInternamentos() {
     this.ngxUiLoaderService.start();
     this.internamentoService.buscarIntenamentos().subscribe({
-      next:(internamentos: Internamento[]) => {
-        this.internamentos = internamentos;
+      next: (internamentos: Internamento[]) => {
+        this.dados = internamentos;
+        this.filtrarLista();
         this.ngxUiLoaderService.stop();
       },
-      error:()=>{
-        this.toastr.error("Erro ao consulta internamentos. Tente novamente mais tarde!","",{progressBar: true});
+      error: () => {
+        this.toastr.error("Erro ao consulta internamentos. Tente novamente mais tarde!", "", { progressBar: true });
         this.ngxUiLoaderService.stop();
       }
     })
   }
+
+  paginarDados() {
+      const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+      const fim = inicio + this.itensPorPagina;
+  
+  
+      this.dadosFiltrados.sort((a, b) => {
+        const eanA = a.medicamento?.ean ?? '';
+        const eanB = b.medicamento?.ean ?? '';
+        return eanA.localeCompare(eanB); // Se quiser ordem numérica real: return +eanA - +eanB;
+      });
+  
+      this.dadosPaginados = this.dadosFiltrados.slice(inicio, fim);
+    }
+  
+    onPaginaAlterada(novaPagina: number) {
+      this.paginaAtual = novaPagina;
+      this.paginarDados();
+    }
+  
+    ngOnChanges() {
+      this.aplicarFiltro();
+    }
+  
+    filtrarLista() {
+      this.paginaAtual = 1;
+      this.aplicarFiltro();
+    }
+  
+    aplicarFiltro() {
+      const texto = this.textoFiltro.toLowerCase();
+  
+      this.dadosFiltrados = this.dados.filter((dado: Internamento) => {
+        const camposParaFiltrar = [
+          dado.leito!.hospital?.razaoSocial || '',
+          dado.leito!.ala?.nome || '',
+          dado.leito?.codigo || '',
+          dado.paciente?.nome || '',
+          dado.paciente?.cpf || '',
+          dado.status || '',
+          dado.medicoSolicitante || '',
+        ];
+  
+        return camposParaFiltrar.some((campo) =>
+          campo.toLowerCase().includes(texto)
+        );
+      });
+  
+      this.paginarDados();
+    }
+  
+    formatarData(dataIso: string): string {
+      const [ano, mes, dia] = dataIso.split('-');
+      return `${dia}/${mes}/${ano}`;
+    }
 
 }
